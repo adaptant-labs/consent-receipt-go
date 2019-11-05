af112e973eca6ffba7ba6a19d8136708bb7cac9c
Paul Mundt <paul.mundt@adaptant.io>
2019-11-05 13:35:24 +0100

    Enable receipt generation via CLI

--- cmd/generate-receipt.go
+++ cmd/generate-receipt.go
@@ -1,17 +1,34 @@
 package cmd
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/adaptant-labs/consent-receipt-go/api"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+func generateJsonReceipt() error {
+	controller := api.NewDataController()
+	cr := api.NewConsentReceipt()
+	cr.AddDataController(controller)
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "\t")
+
+	return encoder.Encode(cr)
+}
 
 // generateReceiptCmd represents the receipt command
 var generateReceiptCmd = &cobra.Command{
 	Use:   "receipt",
 	Short: "Generate a JSON-based Consent Receipt",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("receipt called")
+		err := generateJsonReceipt()
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
